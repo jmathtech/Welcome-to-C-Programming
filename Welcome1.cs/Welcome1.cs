33a491bd8e2ef96690017907e61d99cd51a6f77d
Jamil Matheny <jamil.matheny@gmail.com>
2020-10-05 12:42:00 -0400

    Add project files.

---
+++ Welcome1.cs/Welcome1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Welcome1.cs
+{
+	/* Use a capital letter */
+	public class Welcome1
+	{
+		/* Methods in a class must be called 'Main'; otherwise, the app will not execute.
+		Characters between double quotation marks are strings.
+		Strings are in double quotes.
+
+		 */
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to C# Programing!");
+		} // End Main
+	} // End class Welcome1
+} // End Namespace environment
